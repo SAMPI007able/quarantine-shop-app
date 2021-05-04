bb5d826b27d76c66d0660188af531163934a89c4
SAMPI007able <lahiri.arnab4@gmail.com>
2021-05-04 21:35:22 +0530

    Server changes for hosting angular app in express server
    Server changes for hosting angular app in express server

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var path = require('path');
+var http = require('http');
 
 var port = process.env.PORT || 3001 ;
 var app = express();
@@ -9,10 +10,14 @@ var app = express();
 app.use(bodyParser.json());
 app.use( cors() );
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static(__dirname , path.join('/dist/quarantine-shop-app')));
+app.get('/*', (req, res) => res.sendFile(__dirname) );
+
 var mongoose = require('mongoose');
 const mongoURI = 'mongodb://localhost:27017/quarantine-shop-db';
 mongoose.
-    connect(mongoURI, {useNewUrlParser:true}).
+    connect(mongoURI, {useUnifiedTopology: true, useNewUrlParser:true}).
     then(() => console.log('MongoDB Connected..')).
     catch(err => console.error(err));
 
@@ -21,7 +26,8 @@ app.use('/user', userRoutes);
 const productRoutes = require('./routes/products');
 app.use('/product', productRoutes);
 
-app.listen(port, (req, res) => {
+const server = http.createServer(app);
+server.listen(port, () => {
     console.log('Server is listening.. .');
 })
 
